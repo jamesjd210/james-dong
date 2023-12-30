2b2ae4d610f2dd8e2705de246a3dcb0ef48ca25a
jamesjd210 <jamesjd1024@gmail.com>
2023-12-29 21:52:38 -0600

    metadata change

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: 'Home - Open PRO',
-  description: 'Page description',
+  title: 'James Dong',
+  description: 'James Dong\'s personal website',
 }
 
 import Hero from '@/components/hero'
