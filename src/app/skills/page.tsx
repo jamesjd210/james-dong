9131a7d4f7d73ab101839635ee6df7fd249a4d4a
jamesjd210 <jamesjd1024@gmail.com>
2024-01-17 20:30:23 -0600

    fixed bug with strokeLine and added skills

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
                         {section.items.map((skill, skillIndex) => (
                             <li key={skillIndex} className="bg-gray-300 p-1 my-3 rounded shadow-md flex items-center">
                                 <svg className="w-4 h-4 mr-2 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M5 13l4 4L19 7"></path>
+                                    <path strokeLinecap="round" strokeLinejoin="round" stroke-width="4" d="M5 13l4 4L19 7"></path>
                                 </svg>
                                 {skill}
                             </li>
