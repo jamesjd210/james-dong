84f4a9975d8eeab494dc8d37ebd7944d0e5068a9
jamesjd210 <jamesjd1024@gmail.com>
2024-02-09 18:00:39 -0600

    updated experience page

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -18,11 +18,11 @@ const experiences = [
 
 const Page = () => {
   return (
-    <div className="max-w-3xl mx-auto p-4">
+    <div className="mt-20 max-w-3xl mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">Experience</h1>
       <ul>
         {experiences.map((experience, index) => (
-          <li key={index} className="mb-8">
+          <li key={index} className="mb-8 bg-gray-800 p-4">
             <h2 className="text-xl font-semibold mb-2">{experience.title}</h2>
             <p className="text-gray-400 mb-1">{experience.company} - {experience.year}</p>
             <p className="text-gray-200">{experience.description}</p>
