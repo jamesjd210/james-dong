e128587b661fbf2fe53bcb1fd379acb61aa8cf3c
jamesjd210 <jamesjd1024@gmail.com>
2024-02-15 23:48:29 -0600

    made object to store experiences

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React from "react";
 
-const experiences = [
+interface ExperienceItemProps {
+  title: string;
+  company: string;
+  year: string;
+  description: string;
+}
+
+const experiences: ExperienceItemProps[] = [
   {
     title: 'Associate Software Engineer',
     company: 'Charles Schwab | Full Time',
     year: 'Jan 2023 - Present',
-    description: 'Worked on front-end development using Angular and collaborated with cross-functional teams.'
+    description: 'Worked on front-end development using Angular and collaborated with cross-functional teams.',
   },
   {
-    title: 'Intern',
+    title: 'Software Intern',
     company: 'Charles Schwab',
     year: 'Summer 2022',
-    description: 'Gained hands-on experience in web development and assisted in various projects.'
+    description: `Leveraged JavaScript and Mabl to create 300+ automated tests to confirm code functionality and correctness
+    Reduced overall cost and time of deploying tests for the entire work group by 10% on the CI/CD Bamboo pipeline
+    Documented Mabl integrations with Charles Schwab’s internal network for all future code deployment and tests`,
   },
   // Add more experiences as needed
 ];
@@ -21,7 +30,7 @@ export default function Experience() {
     <div className="mt-20 max-w-3xl mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">Experience</h1>
       <ul>
-        {experiences.map((experience, index) => (
+        {experiences.map((experience: ExperienceItemProps, index: number) => (
           <li key={index} className="mb-8 bg-gray-800 p-4">
             <h2 className="text-xl font-semibold mb-2">{experience.title}</h2>
             <p className="text-gray-400 mb-1">{experience.company} - {experience.year}</p>
@@ -31,4 +40,4 @@ export default function Experience() {
       </ul>
     </div>
   );
-};
+}
